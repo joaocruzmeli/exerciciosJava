dab2585bedd643f5df84336e19e38309f9f2c698
joaocruzmeli <joao.vcruz@mercadolivre.com>
2023-11-03 10:30:52 -0300

    feat: exercicio 15 finalizado

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -2,14 +2,7 @@
 public class TestaConta {
 
     public static void main(String[] args) {
-        Conta c1 = new Conta();
-        Conta c2 = new Conta();
-        Conta c3 = new Conta();
-        Conta c4 = new Conta();
-
-        System.out.println(c1.getIdentificador());
-        System.out.println(c2.getIdentificador());
-        System.out.println(c3.getIdentificador());
-        System.out.println(c4.getIdentificador());
+        Data d1 = new Data(1, 11, 2023);
+        Data d2 = new Data(31, 2, 2023);
     }
 }
