9069565c4eaa3ddb61758692cf3a99d612c11602
joaocruzmeli <joao.vcruz@mercadolivre.com>
2023-11-03 10:33:14 -0300

    feat: exercicio 16 finalizado

---
+++ src/PessoaFisica.java
@@ -0,0 +1,16 @@
+public class PessoaFisica {
+    private String cpf;
+
+    public PessoaFisica (String cpf){
+        validaCpf(cpf);
+        this.cpf = cpf;
+    }
+
+    public void validaCpf(String cpf){
+
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
