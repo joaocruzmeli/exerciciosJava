102663aef927c46e1ffb71ca8a4ab94d51525800
joaocruzmeli <joao.vcruz@mercadolivre.com>
2023-11-03 10:03:47 -0300

    feat: exercicio 4 finalizado

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -2,19 +2,17 @@ public class TestaConta {
 
     public static void main(String[] args) {
         Conta c1 = new Conta();
-
         c1.nomeTitular = "Joao";
-        c1.numero = 123465;
-        c1.agencia = "123";
-        c1.saldo = 2000;
-        c1.dataAbertura = "01/11/2023";
+        c1.saldo = 200;
 
-        c1.recuperarDadosParaImpressao();
+        Conta c2 = new Conta();
+        c2.nomeTitular = "Joao";
+        c2.saldo = 200;
 
-        c1.saca(500);
-        c1.deposita(300);
-
-        double rendimento = c1.calculaRendimento();
-        System.out.println("O rendimento da conta é: R$" + rendimento);
+        if (c1 == c2){
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
     }
 }
