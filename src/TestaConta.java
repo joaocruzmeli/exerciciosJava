ca5f43159e90b0e93719ef0017da4ed56b703336
joaocruzmeli <joao.vcruz@mercadolivre.com>
2023-11-03 10:04:23 -0300

    feat: exercicio 5 finalizado

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -5,9 +5,7 @@ public class TestaConta {
         c1.nomeTitular = "Joao";
         c1.saldo = 200;
 
-        Conta c2 = new Conta();
-        c2.nomeTitular = "Joao";
-        c2.saldo = 200;
+        Conta c2 = c1;
 
         if (c1 == c2){
             System.out.println("iguais");
